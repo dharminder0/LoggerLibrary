b883950c707df7d3c7c84bde21053e9006b6d299
Dharminder Singh <dharminder.singh@fleekitsolutions.com>
2023-11-30 14:53:11 +0530

    Add SQL  Sink configuration

---
+++ CustomLoggerLib/Model/SQLConfigSinkModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLoggerLib.Model {
+    public class SQLConfigSinkModel {
+        public string ConnectionString { get; set; }
+        public string TableName { get; set; }
+        public string LogLevelColumnName { get; set; } = "LogLevel";
+        public string MessageColumnName { get; set; } = "Message";
+        public string NamespaceColumnName { get; set; } = "Namespace";
+    }
+}
---
+++ CustomLoggerLib/Service/Concrete/MSSQLDataSinkService.cs
@@ -0,0 +1,28 @@
+﻿using CustomLoggerLib.Model;
+using CustomLoggerLib.Service.Abstract;
+using System.Data.SqlClient;
+
+namespace CustomLoggerLib.Service.Concrete {
+    public class MSSQLDataSinkService : ILogSinkService {
+        private readonly SQLConfigSinkModel _configSinkModel;
+
+        public MSSQLDataSinkService(SQLConfigSinkModel config) {
+            _configSinkModel = config ?? throw new ArgumentNullException(nameof(config));
+        }
+
+        public void Log(LogMessageModel message) {
+            //using (var connection = new SqlConnection(_configSinkModel.ConnectionString)) {
+            //    var command = connection.CreateCommand();
+            //    command.CommandText = $@"INSERT INTO {_configSinkModel.TableName} 
+            //                          ({_configSinkModel.LogLevelColumnName}, {_configSinkModel.MessageColumnName},{_configSinkModel.NamespaceColumnName}) 
+            //                           VALUES (@LogLevel, @Message, @Namespace)";
+
+            //    command.Parameters.AddWithValue("@LogLevel", message.Level.ToString());
+            //    command.Parameters.AddWithValue("@Message", message.Content);
+            //    command.Parameters.AddWithValue("@Namespace", message.ClassNamespace);
+            //    connection.Open();
+            //    command.ExecuteNonQuery();
+            //}
+        }
+    }
+}
