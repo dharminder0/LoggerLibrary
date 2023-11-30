9738b0ffdec657ff7f7ad0dce07ce66fc2c972de
Dharminder Singh <dharminder.singh@fleekitsolutions.com>
2023-11-30 14:53:41 +0530

    Configure SQL Sink in console

--- LoggerConsole/Program.cs
+++ LoggerConsole/Program.cs
@@ -10,28 +10,32 @@ namespace LoggerConsole
     public class Program {
         public static void Main(string[] args) {
 
-            ////Simple Console Application with CustomLoggerLib integration
-            //LoggerConfiguration config = new LoggerConfiguration();
-            //config.Configure(LogLevel.ERROR, new FileSink("logfile.txt"));
-            //config.Configure(LogLevel.INFO, new ConsoleSink());
-            //Logger logger = config.Logger;
-            //logger.Log(new LogMessage(LogLevel.INFO, "LoggerConsole info message", "LoggerConsole"));
-            //logger.Log(new LogMessage(LogLevel.ERROR, "LoggerConsole error message", "LoggerConsole"));
+            var dbSinkConfig = new SQLConfigSinkModel {
+                ConnectionString ="Data Source=test.database.windows.net;Initial Catalog=Log-Shared-Db-Weu-Test;User Id= Db-User-Test-Acc;Password=M4cc7bbaqXTXj",        // "Your database connection string here",
+                TableName = "LogEntries",
+                LogLevelColumnName = "LogLevel",
+                MessageColumnName = "Message",
+                NamespaceColumnName = "Namespace",
+            };
 
-
-
+            // Register your logger as a singleton
             //webapi/Console Application with IOC pattern with CustomLoggerLib integration
+
             var serviceProvider = new ServiceCollection()
             .AddSingleton<ILoggerService, LoggerService>(serviceProvider => {
                 var loggerConfiguration = new LoggerConfiguration();
                 loggerConfiguration.Configure(LogLevelEnum.ERROR, new FileSinkService("logfile.txt"));
                 loggerConfiguration.Configure(LogLevelEnum.INFO, new ConsoleSinkService());
-
+                loggerConfiguration.Configure(LogLevelEnum.ERROR, new MSSQLDataSinkService(dbSinkConfig));
                 return loggerConfiguration.Logger;
             })
             .BuildServiceProvider();
 
-
+            ////Simple Console Application with CustomLoggerLib integration
+            //LoggerConfiguration config = new LoggerConfiguration();
+            //config.Configure(LogLevel.ERROR, new FileSink("logfile.txt"));
+            //config.Configure(LogLevel.INFO, new ConsoleSink());
+            //Logger logger = config.Logger;
 
             var logger = serviceProvider.GetService<ILoggerService>();
             logger.Log(new LogMessageModel(LogLevelEnum.INFO, "LoggerConsole info message", "LoggerConsole"));
